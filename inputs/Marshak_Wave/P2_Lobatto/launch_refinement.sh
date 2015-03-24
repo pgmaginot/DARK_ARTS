c1d29cb00c541d192903cc72b5c32a1b00e74224
pgmaginot <pmaginot@tamu.edu>
2015-03-24 00:11:17 -0500

    More inputs

---
+++ inputs/Marshak_Wave/P2_Lobatto/launch_refinement.sh
@@ -0,0 +1,29 @@
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_1_Cells_4_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_1_Cells_16_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_1_Cells_64_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_4_Cells_1_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_4_Cells_4_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_4_Cells_16_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_4_Cells_64_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_16_Cells_1_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_16_Cells_4_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_16_Cells_16_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_16_Cells_64_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_64_Cells_1_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_64_Cells_4_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_64_Cells_16_Time.xml
+sleep 2
+bsub -n 1 openmpi-mpirun dark_arts ../inputs/Marshak_Wave/P2_Lobatto/Refine_64_Cells_64_Time.xml
